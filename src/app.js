10080076c6cb040cae43a03da9402bd96197b202
Rohit Karande <rohitkarande543@gmail.com>
2022-11-18 08:48:58 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,28 +1,37 @@
+const express = require("express");
+const app = express();
+const Subscriber = require("./models/subscribers");
 
-const express = require('express');
-const app = express()
+app.get("/subscribers", (req, res) => {
+  Subscriber.find().then((subscribers) => res.send(subscribers));
+  return;
+});
+app.get("/subscribers/names", (req, res) => {
+  Subscriber.find()
+    .select({ name: 1, subscribedChannel: 1 })
+    .then((subscribers) => res.send(subscribers));
+  return;
+});
 
+app.get("/subscribers/:id", (req, res) => {
+  const id = req.params.id;
 
-// Your code goes here
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  Subscriber.find({ _id: id })
+    .then((subscriber) => subscriber.map((subs) => res.send(subs)))
+    .catch((err) => res.status(400).send({ message: err.message }));
+  return;
+});
+/*
+app.get('/subscribers/:id',async(req,res)=>{
+    const id = req.params.id;
+    try{
+    const subscriber = await Subscriber.findById(id);
+    console.log(subscriber);
+    res.send(subscriber);
+    }catch(err){
+        res.status(400).send({message:err.message});
+    }
+  });
+*/
 
 module.exports = app;
